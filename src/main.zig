2b7832fae550bc2df0bb1661c53e2dd1f8b9f498
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-12 19:11:31 +0900

    try using UdpSocket

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,12 @@
 const std = @import("std");
+const net = std.net;
+const UdpSocket = @import("./udp.zig").UdpSocket;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const addr = try net.Address.parseIp4("127.0.0.1", 5555);
+    const sock = try UdpSocket.bind(addr);
+    defer sock.deinit();
+    var buf: [512]u8 = undefined;
+    const recv = try sock.recvFrom(&buf);
+    std.log.info("read: {} bytes. data: {s}\n", .{ recv.num_bytes, buf[0..recv.num_bytes] });
 }

4689733029e988e26cf60d3408ccf9d99e3e06ea
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-13 22:05:25 +0900

    add a function that checks supported QUIC version

---
+++ src/version.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn isSupported(version: u32) bool {
+    const quic_v1 = 0x0000_0001;
+    return version == quic_v1;
+}
+
+test "supported version check" {
+    const draft27 = 0xff00_001b;
+    const draft28 = 0xff00_001c;
+    const draft29 = 0xff00_001d;
+
+    try std.testing.expect(!isSupported(draft27));
+    try std.testing.expect(!isSupported(draft28));
+    try std.testing.expect(!isSupported(draft29));
+    try std.testing.expect(isSupported(0x0000_0001));
+}

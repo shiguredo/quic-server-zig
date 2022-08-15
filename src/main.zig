2c3a910926971caf1aac3415bd457b9b34cd7f80
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-15 09:39:36 +0900

    allocate more buffer

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ pub fn main() !void {
     const addr = try net.Address.parseIp4("127.0.0.1", 5555);
     const sock = try UdpSocket.bind(addr);
     defer sock.deinit();
-    var buf: [512]u8 = undefined;
+    var buf: [65536]u8 = undefined;
 
     while (true) {
         const recv = try sock.recvFrom(&buf);

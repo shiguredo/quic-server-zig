fad2b139748dbfa155ba6df50e8ea0a072720563
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-18 19:37:54 +0900

    rename functions

--- src/crypto.zig
+++ src/crypto.zig
@@ -7,9 +7,9 @@ const VariableLengthVector = @import("./variable_length_vector.zig").VariableLen
 /// Derives server_initial_secret from the given client Destination Connection ID,
 /// writing the result into `out`.
 /// https://www.rfc-editor.org/rfc/rfc9001#name-initial-secrets
-pub fn generateServerInitialSecret(out: *[32]u8, client_destination_connection_id: []const u8) !void {
+pub fn deriveServerInitialSecret(out: *[32]u8, client_destination_connection_id: []const u8) !void {
 
-    const initial_secret = generateCommonInitialSecret(client_destination_connection_id);
+    const initial_secret = deriveCommonInitialSecret(client_destination_connection_id);
     const label = "server in";
     const ctx = "";
     try hkdfExpandLabel(initial_secret, label, ctx, out);
@@ -17,7 +17,7 @@ pub fn generateServerInitialSecret(out: *[32]u8, client_destination_connection_i
 
 /// Derives AEAD Key (key) from the given server_initial_secret,
 /// writing the result into `out`.
-pub fn generateAeadKey(out: *[16]u8, server_initial_secret: [32]u8) !void {
+pub fn deriveAradKey(out: *[16]u8, server_initial_secret: [32]u8) !void {
     const label = "quic key";
     const ctx = "";
     try hkdfExpandLabel(server_initial_secret, label, ctx, out);
@@ -25,7 +25,7 @@ pub fn generateAeadKey(out: *[16]u8, server_initial_secret: [32]u8) !void {
 
 /// Derives Initialization Vector (IV) from the given server_initial_secret,
 /// writing the result into `out`.
-pub fn generateInitializationVector(out: *[12]u8, server_initial_secret: [32]u8) !void {
+pub fn deriveInitializationVector(out: *[12]u8, server_initial_secret: [32]u8) !void {
     const label = "quic iv";
     const ctx = "";
     try hkdfExpandLabel(server_initial_secret, label, ctx, out);
@@ -33,7 +33,7 @@ pub fn generateInitializationVector(out: *[12]u8, server_initial_secret: [32]u8)
 
 /// Derives Header Protection Key (hp) from the given server_initial_secret,
 /// writing the result into `out`.
-pub fn generateHeaderProtectionKey(out: *[16]u8, server_initial_secret: [32]u8) !void {
+pub fn deriveHeaderProtectionKey(out: *[16]u8, server_initial_secret: [32]u8) !void {
     const label = "quic hp";
     const ctx = "";
     try hkdfExpandLabel(server_initial_secret, label, ctx, out);
@@ -96,7 +96,7 @@ fn hkdfExpandLabel(secret: [32]u8, label: []const u8, ctx: []const u8, out: []u8
     HkdfSha256.expand(out, encoded_label[0..encoded_label_size], secret);
 }
 
-fn generateCommonInitialSecret(client_destination_connection_id: []const u8) [32]u8 {
+fn deriveCommonInitialSecret(client_destination_connection_id: []const u8) [32]u8 {
     const initial_salt = [_]u8{
         0x38, 0x76, 0x2c, 0xf7, 0xf5, 0x59, 0x34, 0xb3,
         0x4d, 0x17, 0x9a, 0xe6, 0xa4, 0xc8, 0x0c, 0xad,
@@ -111,7 +111,7 @@ test "initial_secret" {
         0x83, 0x94, 0xc8, 0xf0, 0x3e, 0x51, 0x57, 0x08,
     };
 
-    const got = generateCommonInitialSecret(&client_dcid);
+    const got = deriveCommonInitialSecret(&client_dcid);
     const expected = [_]u8{
         0x7d, 0xb5, 0xdf, 0x06, 0xe7, 0xa6, 0x9e, 0x43,
         0x24, 0x96, 0xad, 0xed, 0xb0, 0x08, 0x51, 0x92,
@@ -127,7 +127,7 @@ test "server_initial_secret" {
     const client_dcid = [_]u8{
         0x83, 0x94, 0xc8, 0xf0, 0x3e, 0x51, 0x57, 0x08,
     };
-    try generateServerInitialSecret(&out, &client_dcid);
+    try deriveServerInitialSecret(&out, &client_dcid);
 
     const expected = [_]u8{
         0x3c, 0x19, 0x98, 0x28, 0xfd, 0x13, 0x9e, 0xfd,
@@ -148,7 +148,7 @@ test "AEAD key" {
         0xfb, 0x82, 0xfa, 0x8d, 0x74, 0x46, 0xfa, 0x7d,
         0x78, 0xbe, 0x80, 0x3a, 0xcd, 0xda, 0x95, 0x1b
     };
-    try generateAeadKey(&out, server_initial_secret);
+    try deriveAradKey(&out, server_initial_secret);
 
     const expected = [_]u8{
         0xcf, 0x3a, 0x53, 0x31, 0x65, 0x3c, 0x36, 0x4c,
@@ -167,7 +167,7 @@ test "Initialization Vector (IV)" {
         0xfb, 0x82, 0xfa, 0x8d, 0x74, 0x46, 0xfa, 0x7d,
         0x78, 0xbe, 0x80, 0x3a, 0xcd, 0xda, 0x95, 0x1b
     };
-    try generateInitializationVector(&out, server_initial_secret);
+    try deriveInitializationVector(&out, server_initial_secret);
 
     const expected = [_]u8{
         0x0a, 0xc1, 0x49, 0x3c, 0xa1, 0x90, 0x58, 0x53,
@@ -186,7 +186,7 @@ test "header protection key" {
         0xfb, 0x82, 0xfa, 0x8d, 0x74, 0x46, 0xfa, 0x7d,
         0x78, 0xbe, 0x80, 0x3a, 0xcd, 0xda, 0x95, 0x1b
     };
-    try generateHeaderProtectionKey(&out, server_initial_secret);
+    try deriveHeaderProtectionKey(&out, server_initial_secret);
 
     const expected = [_]u8{
         0xc2, 0x06, 0xb8, 0xd9, 0xb9, 0xf0, 0xf3, 0x76,

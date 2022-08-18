3b61202bdd1d88cc13e897cecd0b15c0e10ba96c
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-18 19:30:28 +0900

    add generateHeaderProtectionKey function

--- src/crypto.zig
+++ src/crypto.zig
@@ -27,6 +27,12 @@ pub fn generateInitializationVector(out: *[12]u8, server_initial_secret: [32]u8)
     try hkdfExpandLabel(server_initial_secret, label, ctx, out);
 }
 
+pub fn generateHeaderProtectionKey(out: *[16]u8, server_initial_secret: [32]u8) !void {
+    const label = "quic hp";
+    const ctx = "";
+    try hkdfExpandLabel(server_initial_secret, label, ctx, out);
+}
+
 const HkdfLabel = struct {
     length: u16,
     label: VariableLengthVector(u8, label_max_length),
@@ -160,6 +166,25 @@ test "Initialization Vector (IV)" {
     const expected = [_]u8{
         0x0a, 0xc1, 0x49, 0x3c, 0xa1, 0x90, 0x58, 0x53,
         0xb0, 0xbb, 0xa0, 0x3e,
+    };
+
+    try std.testing.expectEqualSlices(u8, &expected, &out);
+}
+
+test "header protection key" {
+    // This test case is brought from https://www.rfc-editor.org/rfc/rfc9001#section-a.1
+    var out: [16]u8 = undefined;
+    const server_initial_secret = [_]u8{
+        0x3c, 0x19, 0x98, 0x28, 0xfd, 0x13, 0x9e, 0xfd,
+        0x21, 0x6c, 0x15, 0x5a, 0xd8, 0x44, 0xcc, 0x81,
+        0xfb, 0x82, 0xfa, 0x8d, 0x74, 0x46, 0xfa, 0x7d,
+        0x78, 0xbe, 0x80, 0x3a, 0xcd, 0xda, 0x95, 0x1b
+    };
+    try generateHeaderProtectionKey(&out, server_initial_secret);
+
+    const expected = [_]u8{
+        0xc2, 0x06, 0xb8, 0xd9, 0xb9, 0xf0, 0xf3, 0x76,
+        0x44, 0x43, 0x0b, 0x49, 0x0e, 0xea, 0xa3, 0x14,
     };
 
     try std.testing.expectEqualSlices(u8, &expected, &out);

b2bfea51c6b38d780392cc0eb85af661698d52c4
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-22 12:37:42 +0900

    add deriveClientInitialSecret function

--- src/crypto.zig
+++ src/crypto.zig
@@ -5,12 +5,31 @@ const HkdfSha256 = std.crypto.kdf.hkdf.HkdfSha256;
 const Aes128 = std.crypto.core.aes.Aes128;
 const VariableLengthVector = @import("./variable_length_vector.zig").VariableLengthVector;
 
+const InitialSecretKind = enum {
+    server,
+    client,
+};
+
 /// Derives server_initial_secret from the given client Destination Connection ID,
 /// writing the result into `out`.
 /// https://www.rfc-editor.org/rfc/rfc9001#name-initial-secrets
 pub fn deriveServerInitialSecret(out: *[32]u8, client_destination_connection_id: []const u8) !void {
+    try deriveInitialSecretInner(.server, out, client_destination_connection_id);
+}
+
+/// Derives client_initial_secret from the given client Destination Connection ID,
+/// writing the result into `out`.
+/// https://www.rfc-editor.org/rfc/rfc9001#name-initial-secrets
+pub fn deriveClientInitialSecret(out: *[32]u8, client_destination_connection_id: []const u8) !void {
+    try deriveInitialSecretInner(.client, out, client_destination_connection_id);
+}
+
+fn deriveInitialSecretInner(comptime kind: InitialSecretKind, out: *[32]u8, client_destination_connection_id: []const u8) !void {
     const initial_secret = deriveCommonInitialSecret(client_destination_connection_id);
-    const label = "server in";
+    const label = switch (kind) {
+        .server => "server in",
+        .client => "client in",
+    };
     const ctx = "";
     try hkdfExpandLabel(initial_secret, label, ctx, out);
 }
@@ -230,6 +249,52 @@ test "server_initial_secret" {
             0x74, 0x6b, 0xf8, 0x30, 0x9d, 0x02, 0xd5, 0xea,
             0x27, 0x15, 0x9c, 0x1e, 0xd6, 0x91, 0x54, 0x03,
             0xb3, 0x63, 0x18, 0xd5, 0xa0, 0x3a, 0xfe, 0xb8,
+            // zig fmt: on
+        };
+
+        try std.testing.expectEqualSlices(u8, &expected, &out);
+    }
+}
+
+test "client_initial_secret" {
+    {
+        // This test case is brought from https://www.rfc-editor.org/rfc/rfc9001#section-a.1
+        var out: [32]u8 = undefined;
+        const client_dcid = [_]u8{
+            // zig fmt: off
+            0x83, 0x94, 0xc8, 0xf0, 0x3e, 0x51, 0x57, 0x08,
+            // zig fmt: on
+        };
+        try deriveClientInitialSecret(&out, &client_dcid);
+
+        const expected = [_]u8{
+            // zig fmt: off
+            0xc0, 0x0c, 0xf1, 0x51, 0xca, 0x5b, 0xe0, 0x75,
+            0xed, 0x0e, 0xbf, 0xb5, 0xc8, 0x03, 0x23, 0xc4,
+            0x2d, 0x6b, 0x7d, 0xb6, 0x78, 0x81, 0x28, 0x9a,
+            0xf4, 0x00, 0x8f, 0x1f, 0x6c, 0x35, 0x7a, 0xea,
+            // zig fmt: on
+        };
+
+        try std.testing.expectEqualSlices(u8, &expected, &out);
+    }
+
+    {
+        // This test case is brought from https://quic.xargs.org/
+        var out: [32]u8 = undefined;
+        const client_dcid = [_]u8{
+            // zig fmt: off
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            // zig fmt: on
+        };
+        try deriveClientInitialSecret(&out, &client_dcid);
+
+        const expected = [_]u8{
+            // zig fmt: off
+            0x47, 0xc6, 0xa6, 0x38, 0xd4, 0x96, 0x85, 0x95,
+            0xcc, 0x20, 0xb7, 0xc8, 0xbc, 0x5f, 0xbf, 0xbf,
+            0xd0, 0x2d, 0x7c, 0x17, 0xcc, 0x67, 0xfa, 0x54,
+            0x8c, 0x04, 0x3e, 0xcb, 0x54, 0x7b, 0x0e, 0xaa,
             // zig fmt: on
         };
 

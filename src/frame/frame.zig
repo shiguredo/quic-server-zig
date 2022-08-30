cb174382f8ae48cee8e89308567987f97167a6fe
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-30 14:46:12 +0900

    start implementing frames

---
+++ src/frame/frame.zig
@@ -0,0 +1,74 @@
+const std = @import("std");
+const Bytes = @import("../bytes.zig").Bytes;
+const Crypto = @import("./crypto.zig").Crypto;
+const Padding = @import("./padding.zig").Padding;
+
+pub const FrameType = enum {
+    padding,
+    crypto,
+    connection_close,
+    // TODO(magurotuna) add other frame types
+
+    const Self = @This();
+
+    pub fn fromInt(type_id: u64) Self {
+        return switch (type_id) {
+            0x00 => .padding,
+            0x06 => .crypto,
+            0x1c...0x1d => .connection_close,
+            // TODO(magurotuna) add other frame types
+            else => unreachable,
+        };
+    }
+};
+
+// TODO(magurotuna): implement
+const ConnectionClose = struct {};
+
+pub const Frame = union(FrameType) {
+    padding: Padding,
+    crypto: Crypto,
+    connection_close: ConnectionClose,
+    // TODO(magurotuna) add other frame types
+
+    const Self = @This();
+
+    pub fn encodedLength(self: Self) usize {
+        return switch (self) {
+            .padding => |p| p.encodedLength(),
+            .crypto => |c| c.encodedLength(),
+            // TODO(magurotuna) implement
+            .connection_close => unreachable,
+            // TODO(magurotuna) add other frame types
+        };
+    }
+
+    pub fn encode(self: Self, out: *Bytes) !void {
+        return switch (self) {
+            .padding => |p| p.encode(out),
+            .crypto => |c| c.encode(out),
+            // TODO(magurotuna) implement
+            .connection_close => unreachable,
+            // TODO(magurotuna) add other frame types
+        };
+    }
+
+    pub fn decode(allocator: std.mem.Allocator, in: *Bytes) !Self {
+        const ty = try in.peekVarInt();
+        return switch (FrameType.fromInt(ty)) {
+            .padding => .{ .padding = try Padding.decode(allocator, in) },
+            .crypto => .{ .crypto = try Crypto.decode(allocator, in) },
+            // TODO(magurotuna) implement
+            .connection_close => unreachable,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        switch (self) {
+            .padding => |p| p.deinit(),
+            .crypto => |c| c.deinit(),
+            // TODO(magurotuna) implement
+            .connection_close => unreachable,
+        }
+    }
+};

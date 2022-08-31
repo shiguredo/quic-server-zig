79b7edee301c212d79a4fbc45f33d19b7acaac89
Yusuke Tanaka <yusuktan@maguro.dev>
2022-08-31 17:24:10 +0900

    try adding basic utilities as other packages in build.zig

--- build.zig
+++ build.zig
@@ -14,6 +14,7 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("quic-server-zig", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    addInternalPackages(exe);
     exe.install();
 
     const run_cmd = exe.run();
@@ -28,7 +29,32 @@ pub fn build(b: *std.build.Builder) void {
     const exe_tests = b.addTest("src/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
+    addInternalPackages(exe_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
+}
+
+fn pkgPath(comptime pathRelativeToProjectRoot: []const u8) std.build.FileSource {
+    const root = comptime std.fs.path.dirname(@src().file).?;
+    return .{
+        .path = root ++ std.fs.path.sep_str ++ pathRelativeToProjectRoot,
+    };
+}
+
+fn addInternalPackages(step: *std.build.LibExeObjStep) void {
+    step.addPackage(.{
+        .name = "variable_length_vector",
+        .source = pkgPath("src/variable_length_vector.zig"),
+    });
+
+    step.addPackage(.{
+        .name = "bytes",
+        .source = pkgPath("src/bytes.zig"),
+    });
+
+    step.addPackage(.{
+        .name = "utils",
+        .source = pkgPath("src/utils.zig"),
+    });
 }

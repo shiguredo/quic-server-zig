f7b7a4b8b4e375e83e5940a0e45af9a9b6028026
Yusuke Tanaka <yusuktan@maguro.dev>
2022-09-06 23:48:12 +0900

    add blank encode methods to all packet types

--- src/packet/version_negotiation.zig
+++ src/packet/version_negotiation.zig
@@ -1,2 +1,13 @@
+const std = @import("std");
+const Bytes = @import("../bytes.zig").Bytes;
+
 // TODO(magurotuna)
-pub const VersionNegotiation = struct {};
+pub const VersionNegotiation = struct {
+    const Self = @This();
+
+    pub fn encode(self: Self, out: *Bytes) !void {
+        // TODO(magurotuna)
+        _ = self;
+        _ = out;
+    }
+};

1715945715eee359f821150afab8faffbc716aac
Yusuke Tanaka <yusuktan@maguro.dev>
2022-09-14 23:25:49 +0900

    add pkt_num_spaces to Conn

--- src/conn.zig
+++ src/conn.zig
@@ -1,39 +1,27 @@
 const std = @import("std");
-const net = std.net;
 const ArrayList = std.ArrayList;
-const Bytes = @import("./bytes.zig").Bytes;
-const Initial = @import("./packet/initial.zig").Initial;
-const quic_v1 = @import("./version.zig").quic_v1;
+const PacketNumberSpaces = @import("./packet_number_space.zig").PacketNumberSpaces;
 
 pub const Conn = struct {
-    source_connection_id: ArrayList(u8),
-    destination_connection_id: ArrayList(u8),
+    scid: ArrayList(u8),
+    dcid: ArrayList(u8),
+    pkt_num_spaces: PacketNumberSpaces,
 
     const Self = @This();
 
-    /// Accepts the connection from a client and creates a new `Conn` instance to manage its state.
-    pub fn accept(
-        allocator: std.mem.Allocator,
-        source_connection_id: []const u8,
-        original_destination_connection_id: []const u8,
-        local: net.Address,
-        peer: net.Address,
-    ) !Self {
-        var scid = try ArrayList(u8).initCapacity(allocator, source_connection_id.len);
-        errdefer scid.deinit();
-        scid.appendSliceAssumeCapacity(source_connection_id);
+    pub fn new(allocator: std.mem.Allocator, scid: []const u8, dcid: []const u8) !Self {
+        var scid_owned = try ArrayList(u8).initCapacity(allocator, scid.len);
+        errdefer scid_owned.deinit();
+        scid_owned.appendSliceAssumeCapacity(scid);
 
-        var odcid = try ArrayList(u8).initCapacity(allocator, original_destination_connection_id.len);
-        errdefer odcid.deinit();
-        odcid.appendSliceAssumeCapacity(original_destination_connection_id);
-
-        // TODO(magurotuna): use local and peer address information
-        _ = local;
-        _ = peer;
+        var dcid_owned = try ArrayList(u8).initCapacity(allocator, dcid.len);
+        errdefer dcid_owned.deinit();
+        dcid_owned.appendSliceAssumeCapacity(dcid);
 
         return Self{
-            .source_connection_id = scid,
-            .original_destination_connection_id = odcid,
+            .scid = scid_owned,
+            .dcid = dcid_owned,
+            .pkt_num_spaces = PacketNumberSpaces.initFill(.{}),
         };
     }
 
@@ -42,17 +30,8 @@ pub const Conn = struct {
         self.destination_connection_id.deinit();
     }
 
-    /// Processes QUIC packets received from the peer, returning the number of bytes processed.
-    /// `buf` is expected to be the payload of UDP.
-    pub fn recv(self: *Self, buf: []u8) !usize {
-        // TODO(magurotuna): `buf` may contain multiple QUIC packets, which referred to as "coalesced",
-        // but for now those cases are not handled properly.
-
+    pub fn doHandshake(self: *Self) void {
+        // TODO
         _ = self;
-        _ = buf;
-
-        // if (!self.handshake_completed) {
-        //     self.do_handshake();
-        // }
     }
 };

9685db09d674aaa9c30b25818e15d24666a576b9
Yusuke Tanaka <yusuktan@maguro.dev>
2022-09-20 00:29:47 +0900

    add RecvBuf, SendBuf, and Stream as incomplete ones

--- src/stream.zig
+++ src/stream.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const mem = std.mem;
+const math = std.math;
 const Allocator = mem.Allocator;
+const PriorityQueue = std.PriorityQueue;
+const Deque = @import("./deque.zig").Deque;
 
 /// This type represents a split chunk of a QUIC's stream.
 pub const RangeBuf = struct {
@@ -32,4 +35,41 @@ pub const RangeBuf = struct {
     pub fn deinit(self: Self) void {
         self.allocator.free(self.data);
     }
+};
+
+fn rangeBufCompare(context: void, a: RangeBuf, b: RangeBuf) math.Order {
+    _ = context;
+    return math.order(a.offset, b.offset);
+}
+
+pub const RecvBuf = struct {
+    /// Chunks of data received from the peer that have not yet been read by
+    /// the application, ordered by offset.
+    data: RangeBufMinHeap,
+    /// The total length of data received on this stream.
+    length: usize,
+
+    const RangeBufMinHeap = PriorityQueue(RangeBuf, void, rangeBufCompare);
+    const Self = @This();
+};
+
+pub const SendBuf = struct {
+    /// Chunks of data to be sent to the peer.
+    data: SendQueue,
+    /// The amount of data currently buffered.
+    length: usize,
+
+    const SendQueue = Deque(RangeBuf);
+    const Self = @This();
+};
+
+pub const Stream = struct {
+    /// Receive-side stream buffer.
+    recv: RecvBuf,
+    /// Send-side stream buffer.
+    send: SendBuf,
+    /// Whether the stream is bidirectional.
+    bidi: bool,
+    /// Whether the stream was created by the local endpoint.
+    local: bool,
 };

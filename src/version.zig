4a279f3be462e0412e33bb55d9264cf7519084fd
Yusuke Tanaka <yusuktan@maguro.dev>
2022-09-22 00:57:38 +0900

    add TLS v1.3 identifier

--- src/version.zig
+++ src/version.zig
@@ -16,3 +16,9 @@ test "supported version check" {
     try std.testing.expect(!isSupported(draft29));
     try std.testing.expect(isSupported(0x0000_0001));
 }
+
+// https://www.rfc-editor.org/rfc/rfc8446.html#section-4.2.1
+//
+// > A server which negotiates TLS 1.3 MUST respond by sending a "supported_versions"
+// > extension containing the selected version value (0x0304).
+pub const tls_v1_3 = 0x0304;
